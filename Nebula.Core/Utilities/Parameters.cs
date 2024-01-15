7db8311ae6bbd1b5a110441e68ce84ff919d01e6
Yunivers <92553545+AITYunivers@users.noreply.github.com>
2024-01-14 21:21:59 -0700

    Fixed images not decompiling.

--- Nebula.Core/Utilities/Parameters.cs
+++ Nebula.Core/Utilities/Parameters.cs
@@ -2,7 +2,7 @@
 {
     public static class Parameters
     {
-        public static bool DontIncludeImages = true;
+        public static bool DontIncludeImages = false;
         public static bool DontIncludeFonts = false;
         public static bool DontIncludeSounds = false;
     }
