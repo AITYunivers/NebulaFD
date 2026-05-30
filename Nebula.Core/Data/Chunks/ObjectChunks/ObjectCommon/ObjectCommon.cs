cd758d0a88eb24d207abfd388aa4b2e5d5b667d7
EX0RCIST <joeydrew633@gmail.com>
2026-05-30 08:14:07 +0300

    reverted previous changes, did different ones
    reverted changes with GetObject(), created 2 public static strings for getting qualifier's name in ObjectCommon and moved GetQualifierName method to ObjectCommon
    these changes do the same thing as in previous commit

--- Nebula.Core/Data/Chunks/ObjectChunks/ObjectCommon/ObjectCommon.cs
+++ Nebula.Core/Data/Chunks/ObjectChunks/ObjectCommon/ObjectCommon.cs
@@ -4,6 +4,7 @@ using Nebula.Core.Data.Chunks.FrameChunks.Events.Parameters;
 using Nebula.Core.Memory;
 using System.Diagnostics;
 using System.Drawing;
+using Nebula.Core.Data.Chunks.AppChunks;
 
 namespace Nebula.Core.Data.Chunks.ObjectChunks.ObjectCommon
 {
@@ -238,36 +239,162 @@ namespace Nebula.Core.Data.Chunks.ObjectChunks.ObjectCommon
 
         }
 
-        // method for getting object/qualifier in ACEventBase
-        public static ObjectInfo? GetObjectACEventBase(ushort ObjectInfo, short ObjectType, ACEventBase ace)
+        // method for getting qualifiers in ACE
+        public static string TryGetQualifierACE(ACEventBase ace, ushort ObjectInfo, short ObjectType)
         {
-            if (NebulaCore.Windows && NebulaCore.Build >= 296 && NebulaCore.Fusion >= 2.5 && (ObjectInfo & 0x8000) != 0)
+            if (ace.Parent?.Parent.Qualifiers.Where(q => q.ObjectInfo == ObjectInfo && q.Type == ObjectType).Any() != true)
             {
-                if (ace.Parent?.Parent.Qualifiers.Where(x => x.ObjectInfo == ObjectInfo && x.Type == ObjectType).Any() != true)
-                    ace.Parent?.Parent.Qualifiers.Add(new Qualifier() { ObjectInfo = ObjectInfo, Type = ObjectType });
+                if ((ObjectInfo & 0x8000) != 0)
+                    ace.Parent?.Parent.Qualifiers.Add(new Qualifier() { ObjectInfo = ObjectInfo, Type = ObjectType}); 
             }
-            if (ace.Parent?.Parent.Qualifiers.Where(x => x.ObjectInfo == ObjectInfo).Any() == true)
-                return null;
-            else if (NebulaCore.MFA && ace.Parent?.Parent.EventObjects.Count > 0)
-                return NebulaCore.PackageData.FrameItems.Items[(int)ace.Parent.Parent.EventObjects[ObjectInfo].ItemHandle];
-            else
-                return NebulaCore.PackageData.FrameItems.Items[ObjectInfo];
+            Qualifier[] qualifiers = ace.Parent?.Parent.Qualifiers.Where(x => x.ObjectInfo == ObjectInfo && x.Type == ObjectType).ToArray()!;
+            if (qualifiers.Length > 0)
+                return GetQualifierName(qualifiers.First());
+            return "Unknown Object";
         }
 
-        //method for getting object/qualifier in different places
-        public static ObjectInfo? GetObject(ushort ObjectInfo, short ObjectType, ParameterChunk paramChunk)
+        // method for getting qualifiers in different places
+        public static string TryGetQualifier(ParameterChunk paramChunk, ushort ObjectInfo, short ObjectType)
         {
-            if (NebulaCore.Windows && NebulaCore.Build >= 296 && NebulaCore.Fusion >= 2.5 && (ObjectInfo & 0x8000) != 0)
+            if (paramChunk.Parent?.FrameEvents?.Qualifiers.Where(q => q.ObjectInfo == ObjectInfo && q.Type == ObjectType).Any() != true)
             {
-                if (paramChunk.Parent?.FrameEvents?.Qualifiers.Where(x => x.ObjectInfo == ObjectInfo && x.Type == ObjectType).Any() != true)
+                if ((ObjectInfo & 0x8000) != 0)
                     paramChunk.Parent?.FrameEvents?.Qualifiers.Add(new Qualifier() { ObjectInfo = ObjectInfo, Type = ObjectType });
             }
-            if (paramChunk.Parent?.FrameEvents?.Qualifiers.Where(x => x.ObjectInfo == ObjectInfo).Any() == true)
-                return null;
-            else if (NebulaCore.MFA && paramChunk.Parent?.FrameEvents?.EventObjects.Count > 0)
-                return NebulaCore.PackageData.FrameItems.Items[(int)paramChunk.Parent.FrameEvents.EventObjects[ObjectInfo].ItemHandle];
+            Qualifier[] qualifiers = paramChunk.Parent?.FrameEvents?.Qualifiers.Where(x => x.ObjectInfo == ObjectInfo && x.Type == ObjectType).ToArray()!;
+            if (qualifiers.Length > 0)
+                return GetQualifierName(qualifiers.First());
+            return "Unknown Object";
+        }
+
+        public static string GetQualifierName(Qualifier qualifier)
+        {
+            string qualifierName = "Group." + (qualifier.ObjectInfo & 0x7FFF) switch
+            {
+                0 => "Player",
+                1 => "Good",
+                2 => "Neutral",
+                3 => "Bad",
+                4 => "Enemies",
+                5 => "Friends",
+                6 => "Bullets",
+                7 => "Arms",
+                8 => "Bonus",
+                9 => "Collectables",
+                10 => "Traps",
+                11 => "Doors",
+                12 => "Keys",
+                13 => "Texts",
+                14 => "0",
+                15 => "1",
+                16 => "2",
+                17 => "3",
+                18 => "4",
+                19 => "5",
+                20 => "6",
+                21 => "7",
+                22 => "8",
+                23 => "9",
+                24 => "Parents",
+                25 => "Children",
+                26 => "Data",
+                27 => "Timed",
+                28 => "Engine",
+                29 => "Areas",
+                30 => "Reference Points",
+                31 => "Radar Enemies",
+                32 => "Radar Friends",
+                33 => "Radar Neutrals",
+                34 => "Music",
+                35 => "Sound",
+                36 => "Waveform",
+                37 => "Background Scenery",
+                38 => "Foreground Scenery",
+                39 => "Decorations",
+                40 => "Water",
+                41 => "Clouds",
+                42 => "Empty",
+                43 => "Fog",
+                44 => "Flowers",
+                45 => "Animals",
+                46 => "Bosses",
+                47 => "NPC",
+                48 => "Vehicles",
+                49 => "Rockets",
+                50 => "Balls",
+                51 => "Bombs",
+                52 => "Explosions",
+                53 => "Particles",
+                54 => "Clothes",
+                55 => "Glow",
+                56 => "Arrows",
+                57 => "Buttons",
+                58 => "Cursors",
+                59 => "Drawing Tools",
+                60 => "Indicator",
+                61 => "Shapes",
+                62 => "Shields",
+                63 => "Shifting Blocks",
+                64 => "Magnets",
+                65 => "Negative Matter",
+                66 => "Neutral Matter",
+                67 => "Positive Matter",
+                68 => "Breakable",
+                69 => "Dissolving",
+                70 => "Dialogue",
+                71 => "HUD",
+                72 => "Inventory",
+                73 => "Inventory Item",
+                74 => "Interface",
+                75 => "Movable",
+                76 => "Perspective",
+                77 => "Calculation Objects",
+                78 => "Invisible",
+                79 => "Masks",
+                80 => "Obstacles",
+                81 => "Value Holder",
+                82 => "Helpful",
+                83 => "Powerups",
+                84 => "Targets",
+                85 => "Trapdoors",
+                86 => "Dangers",
+                87 => "Forbidden",
+                88 => "Physical objects",
+                89 => "3D Objects",
+                90 => "Generic 1",
+                91 => "Generic 2",
+                92 => "Generic 3",
+                93 => "Generic 4",
+                94 => "Generic 5",
+                95 => "Generic 6",
+                96 => "Generic 7",
+                97 => "Generic 8",
+                98 => "Generic 9",
+                99 => "Generic 10",
+                _ => string.Empty
+            };
+
+            qualifierName += "." + qualifier.Type switch
+            {
+                2 => "Sprite",
+                3 => "Text",
+                4 => "Question",
+                5 => "Score",
+                6 => "Lives",
+                7 => "Counter",
+                _ => string.Empty
+            };
+
+            if (qualifier.Type >= 32 && NebulaCore.PackageData.Extensions.Exts.ContainsKey(qualifier.Type - 32))
+            {
+                Extension ext = NebulaCore.PackageData.Extensions.Exts[qualifier.Type - 32];
+                qualifierName += ext.Name;
+            }
+
+            if (qualifierName.StartsWith("Group.."))
+                return "Unknown Qualifier";
             else
-                return NebulaCore.PackageData.FrameItems.Items[ObjectInfo];
+                return qualifierName;
         }
 
         public void GetOffset(ByteReader reader, int index, bool check = false)
